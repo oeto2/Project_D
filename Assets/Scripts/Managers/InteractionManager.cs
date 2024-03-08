740851761264d87fa7eb4e7cd5bd9f064baa6ba9
박지훈 <150890216+art-riel@users.noreply.github.com>
2024-03-08 20:53:00 +0900

    feat : 상호작용 구현 및 포탈 상호작용
    상호작용 구현
    포탈cs 상호작용으로구현

---
+++ Assets/Scripts/Managers/InteractionManager.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+
+public interface IInteractable
+{
+    string GetInteractPrompt();
+    void OnInteract();
+    void CancelInteract();
+}
+
+
+public class InteractionManager : MonoBehaviour
+{
+    public float checkRate = 0.05f;
+    private float lastCheckTime;
+    public float maxCheckDistance;
+    public LayerMask layerMask;
+
+    private GameObject curInteractGameObject;
+    private IInteractable curInteractable;
+
+    public TextMeshProUGUI promptText;
+    private Camera camera;
+
+    private bool _isInteract= false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - lastCheckTime > checkRate)
+        {
+            lastCheckTime = Time.time;
+
+            Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
+            {
+                if (hit.collider.gameObject != curInteractGameObject)
+                {
+                    curInteractGameObject = hit.collider.gameObject;
+                    curInteractable = hit.collider.GetComponent<IInteractable>();
+                    SetPromptText();
+                }
+            }
+            else
+            {
+                curInteractGameObject = null;
+                curInteractable = null;
+                promptText.gameObject.SetActive(false);
+            }
+        }
+        if (_isInteract && curInteractable != null)
+        {
+            curInteractable.OnInteract();
+        }
+    }
+
+    private void SetPromptText()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = string.Format("<b>[E]</b> {0}", curInteractable.GetInteractPrompt());
+        
+    }
+
+    //매프레임 입력을 받지 않음
+    public void OnIteractInput(InputAction.CallbackContext callbackcontext)
+    {
+        if (callbackcontext.phase == InputActionPhase.Started && curInteractable != null)
+        {
+            _isInteract = true;
+            //curInteractable.OnInteract();
+            //curInteractGameObject = null;
+            //curInteractable = null;
+            //promptText.gameObject.SetActive(false);
+        }
+        else if (callbackcontext.phase == InputActionPhase.Canceled && curInteractable != null)
+        {
+            _isInteract = false;
+            curInteractable.CancelInteract();
+        }
+    }
+}
--- Assets/Scripts/Portal/Portal.cs
+++ Assets/Scripts/Portal/Portal.cs
@@ -4,45 +4,71 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Portal : MonoBehaviour
+public class Portal : MonoBehaviour, IInteractable
 {
     private float _time;
-    public GameObject portalUI;
     public Slider loadingBar;
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("TriggerOn");
-        portalUI.SetActive(true);
-        if (other.gameObject.CompareTag("Player"))
-        {
-            portalUI.SetActive(true);
-        }
-    }
-    private void OnTriggerStay(Collider other)
-    {
-        //F키를 눌렀는지 확인
-        if (Input.GetKey(KeyCode.E))
-        {
-            _time += Time.deltaTime;
-            loadingBar.value = _time;
-            Debug.Log(_time);
-            if (_time >= 3)
-            {
-                //플레이어 씬이동
+    //private void Start()
+    //{
+    //    loadingBar = GameObject.Find("LoadingBar").GetComponent<Slider>();
+    //}
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log("TriggerOn");
+    //    portalUI.SetActive(true);
+    //    if (other.gameObject.CompareTag("Player"))
+    //    {
+    //        portalUI.SetActive(true);
+    //    }
+    //}
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (Input.GetKey(KeyCode.E))
+    //    {
+    //        _time += Time.deltaTime;
+    //        loadingBar.value = _time / 3;
+    //        Debug.Log(_time);
+    //        if (_time >= 3)
+    //        {
+    //            //플레이어 씬이동
 
-            }
-        }
-        else
+    //        }
+    //    }
+    //    else
+    //    {
+    //        _time = 0;
+    //        loadingBar.value = _time / 3;
+
+    //    }
+    //}
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    Debug.Log("ExitTrigger");
+    //    _time = 0;
+    //    loadingBar.value = _time / 3;
+    //    portalUI.SetActive(false);
+    //}
+
+    string IInteractable.GetInteractPrompt()
+    {
+        return string.Format("Use Portal");
+    }
+
+    void IInteractable.OnInteract()
+    {
+        loadingBar.gameObject.SetActive(true);
+        _time += Time.deltaTime;
+        loadingBar.value = _time / 3;
+        if (_time >= 3)
         {
-            _time = 0;
-            loadingBar.value = _time;
+            //플레이어 씬이동
+
         }
     }
-    private void OnTriggerExit(Collider other)
+    void IInteractable.CancelInteract()
     {
-        Debug.Log("ExitTrigger");
+        loadingBar.gameObject.SetActive(false);
         _time = 0;
-        loadingBar.value = _time;
-        portalUI.SetActive(false);
+        loadingBar.value = _time / 3;
     }
 }
