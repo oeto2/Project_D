0b8d86b59eb220bce89d3ca46539023453401420
oeto2 <oeto2@naver.com>
2024-03-08 17:21:10 +0900

    Feat : 몬스터 상태머신 틀 구현

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [field: Header("References")]
+    
+    //몬스터에 대한 데이터
+    [field: SerializeField] public EnemySO Data { get; private set; }
+
+    [field: Header("Animations")]
+    [field: SerializeField] public EnemyAnimationData AnimationData { get; private set; }
+    public Rigidbody Rigidbody { get; private set; }
+    public Animator Animator { get; private set; }
+    public ForceReceiver ForceReceiver { get; private set; }
+    public CharacterController Controller { get; private set; }
+
+    private EnemyStateMachine stateMachine;
+
+
+    void Awake()
+    {
+        //애니메이션 데이터 할당
+        AnimationData.Initialize();
+
+        Rigidbody = GetComponent<Rigidbody>();
+        Animator = GetComponentInChildren<Animator>();
+        Controller = GetComponent<CharacterController>();
+        ForceReceiver = GetComponent<ForceReceiver>();
+
+        //stateMachine = new EnermyStateMachine(this);
+    }
+
+    private void Start()
+    {
+        stateMachine.ChangeState(stateMachine.IdlingState);
+    }
+
+    private void Update()
+    {
+        stateMachine.HandleInput();
+
+        stateMachine.Update();
+    }
+
+    private void FixedUpdate()
+    {
+        stateMachine.PhysicsUpdate();
+    }
+}
