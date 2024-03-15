98ac4c234a001991e70a383c6f6415564d9dfb05
Sinsa9706 <ox63023@gmail.com>
2024-03-15 21:03:35 +0900

    Feat : 몬스터 DB 관련 script 및 excel 추가

---
+++ Assets/RawData/MonsterDBSheet.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExcelAsset(AssetPath = "Resources/DB", ExcelName = "MonsterDBSheet")]
+public class MonsterDBSheet : ScriptableObject
+{
+	//public List<EntityType> Monster_Table; // Replace 'EntityType' to an actual type that is serializable.
+}
---
+++ Assets/Scripts/KKC/MonsterData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Constants;
+
+public class MonsterData : MonoBehaviour
+{
+    [SerializeField] private int Id;
+    [SerializeField] private string Monster_Name;
+    [SerializeField] private float Monster_HP;
+    [SerializeField] private float Monster_MP;
+    [SerializeField] private float Monster_Atk;
+    [SerializeField] private float Monster_Atk_Spd;
+    [SerializeField] private float Monster_Def;
+    [SerializeField] private float Monster_Speed;
+    [SerializeField] private int Monster_Max_Root;
+    
+}
