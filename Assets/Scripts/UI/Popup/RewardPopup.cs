50df48c63f35f203717c540448b2534ff8f185bf
oeto2 <oeto2@naver.com>
2024-03-21 19:49:18 +0900

    Fix : 이벤트 시스템 추가

--- Assets/Scripts/UI/Popup/RewardPopup.cs
+++ Assets/Scripts/UI/Popup/RewardPopup.cs
@@ -13,6 +13,5 @@ public class RewardPopup : UIBase
         Cursor.lockState = CursorLockMode.None;
         //씬 이동
         _enterButton.onClick.AddListener(() => GameManager.Instance.ChangeScene(SceneType.LobbyScene));
-        _enterButton.onClick.AddListener(() => Debug.Log("버튼 클릭"));
     }
 }
