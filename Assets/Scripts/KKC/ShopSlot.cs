ecf214b19ca2b511ac9588b0d0e727ca64c2b002
Sinsa9706 <ox63023@gmail.com>
2024-03-22 17:53:36 +0900

    Feat : 경고 창 추가

--- Assets/Scripts/KKC/ShopSlot.cs
+++ Assets/Scripts/KKC/ShopSlot.cs
@@ -32,10 +32,10 @@ public class ShopSlot : MonoBehaviour
         int itemCount; // 아이템 갯수
         if (int.TryParse(ItemStack.text, out itemCount))
         {
-            //Inventory.Instance.AcquireItem(ItemData, itemCount);
-            Debug.Log(ItemData.itemName + " " + itemCount);
+            //Inventory.instance.AcquireItem(ItemData, itemCount);
         }
         ItemStack.text = null;
+
         //else if() // 골드 부족할 때
     }
 }
