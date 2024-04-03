625307cb925f8b095dc4b9e7f5244587ec8ee186
oeto2 <oeto2@naver.com>
2024-04-03 15:37:26 +0900

    Chore : 상호작용 시스템을 몬스터 데이터 기반으로 구조 변경

--- Assets/Scripts/Enemy/EnemyInteration.cs
+++ Assets/Scripts/Enemy/EnemyInteration.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class EnemyInteration : MonoBehaviour, IInteractable
 {
+    private Enemy _enemy;
+
+    private void Awake()
+    {
+        _enemy = GetComponentInParent<Enemy>();
+    }
+
     public void CancelInteract()
     {
         //상호작용 취소
@@ -12,7 +19,7 @@ public class EnemyInteration : MonoBehaviour, IInteractable
     public string GetInteractPrompt()
     {
         //상호작용 글씨
-        return string.Format("스켈레톤");
+        return string.Format(_enemy.Data.monsterName);
     }
 
     public void OnInteract()
