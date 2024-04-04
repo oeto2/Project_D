66c20c1b68d3f49f7f2f5de829298547e33c883b
Sinsa9706 <ox63023@gmail.com>
2024-04-04 22:35:08 +0900

    fix : 재사용 스크롤 뷰 수정 (완료)

--- Assets/Scripts/UI/Popup/ShopPopup.cs
+++ Assets/Scripts/UI/Popup/ShopPopup.cs
@@ -7,15 +7,15 @@ using UnityEngine.UI;
 
 public class ShopPopup : UIBase
 {
-    [field: SerializeField] public ShopListSO ShopItems;
-    [SerializeField] private Transform _shopItemSlots;
+    //[field: SerializeField] public ShopListSO ShopItems;
+    //[SerializeField] private Transform _shopItemSlots;
 
     private GameObject lobbyUpPopup_Object;
 
-    private void Start()
-    {
-        ShopItemSet();
-    }
+    //private void Start()
+    //{
+    //    ShopItemSet();
+    //}
 
     private void Awake()
     {
@@ -35,14 +35,14 @@ public class ShopPopup : UIBase
     }
 
     // 상점에 SO에 존재하는 int값과 같은 id의 아이템 추가
-    private void ShopItemSet()
-    {
-        foreach (var shopItemId in ShopItems.ShopItems)
-        {
-            var shopItem = Database.Item.Get(shopItemId);
-            var shopSlot = ResourceManager.Instance.Instantiate("UI/ShopSlot", _shopItemSlots);
-            shopSlot.GetComponent<ShopSlot>().UpdateUI(shopItem);
-            //Debug.Log(Database.Item.Get(shopItemId).itemName);
-        }
-    }
+    //private void ShopItemSet()
+    //{
+    //    foreach (var shopItemId in ShopItems.ShopItems)
+    //    {
+    //        var shopItem = Database.Item.Get(shopItemId);
+    //        var shopSlot = ResourceManager.Instance.Instantiate("UI/ShopSlot", _shopItemSlots);
+    //        shopSlot.GetComponent<ShopSlot>().UpdateUI(shopItem);
+    //        //Debug.Log(Database.Item.Get(shopItemId).itemName);
+    //    }
+    //}
 }
