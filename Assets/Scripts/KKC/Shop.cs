644059e0b7355fcd93cfae306e42ee06fbadd82d
Sinsa9706 <ox63023@gmail.com>
2024-04-05 22:01:48 +0900

    fix : 상점 아이템 리스트 자동화 (완료)

--- Assets/Scripts/KKC/Shop.cs
+++ Assets/Scripts/KKC/Shop.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 public class Shop : UIRecycleViewController<ItemData>
 {
     [field: SerializeField] public ShopListSO ShopItems;
+    private List<int> ShopItemsList;
+   
+    private int _shopCode = 10000001;
 
     // 리스트 항목의 데이터를 읽어 들이는 메서드
     protected override void Start()
     {
         base.Start();
 
+        LoadShopItemData();
         ShopItemSet();
+    }
+
+    private void LoadShopItemData()
+    {
+        var shopData = Database.Shop.Get(_shopCode);
+        ShopItemsList = shopData.ShopItemList;
     }
 
     private void ShopItemSet()
     {
         tableData = new List<ItemData>();
-        foreach (var shopItemId in ShopItems.ShopItems)
+        foreach (var shopItemId in ShopItemsList)
         {
             var shopItem = Database.Item.Get(shopItemId);
             tableData.Add(shopItem);
--- Assets/Scripts/KKC/ShopData.cs
+++ Assets/Scripts/KKC/ShopData.cs
@@ -12,25 +12,35 @@ public class ShopData
     [SerializeField] private int _item3;
     [SerializeField] private int _item4;
     [SerializeField] private int _item5;
+    [SerializeField] private int _item6;
+    [SerializeField] private int _item7;
+    [SerializeField] private int _item8;
+    [SerializeField] private int _item9;
+    [SerializeField] private int _item10;
 
     public int id => _id;
     public string shopName => _shopName;
 
-    private List<ShopItem> shopItemList;
+    private List<int> shopItemList;
 
-    public List<ShopItem> ShopItemList
+    public List<int> ShopItemList
     {
         get
         {
             if(shopItemList == null)
             {
-                shopItemList = new List<ShopItem>();
+                shopItemList = new List<int>();
 
                 CheckShopItem(_item1);
                 CheckShopItem(_item2);
                 CheckShopItem(_item3);
                 CheckShopItem(_item4);
                 CheckShopItem(_item5);
+                CheckShopItem(_item6);
+                CheckShopItem(_item7);
+                CheckShopItem(_item8);
+                CheckShopItem(_item9);
+                CheckShopItem(_item10);
             }
 
             return shopItemList;
@@ -41,17 +51,7 @@ public class ShopData
     {
         if(shopItemId_ != 0)
         {
-            shopItemList.Add(new ShopItem(shopItemId_));
+            shopItemList.Add(shopItemId_);
         }
-    }
-}
-
-public class ShopItem
-{
-    public int item { get; }
-
-    public ShopItem(int item_)
-    {
-        item = item_;
     }
 }
