13b4b0519c450b3ad396f800a16396b8e37000a7
oeto2 <oeto2@naver.com>
2024-04-08 23:11:56 +0900

    Feat : 데미지 UI 추가 및 풀매니저 추가

---
+++ Assets/Scripts/UI/Battle/HitDamageText.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+
+public class HitDamageText : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Camera.main.transform.rotation;
+    }
+}
+ 
