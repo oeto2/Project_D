876fcee54ed933d8266ebc4ac89b5ceffcb8a4b8
oeto2 <oeto2@naver.com>
2024-04-18 18:18:37 +0900

    Feat : 오크 스킬 구현완료

--- Assets/Scripts/Enemy/StateMachine/EnemyAttackState.cs
+++ Assets/Scripts/Enemy/StateMachine/EnemyAttackState.cs
@@ -1,3 +1,4 @@
+using Constants;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,57 @@ public class EnemyAttackState : EnemyBaseState
 {
     private bool alreadyAppliedForce;
 
+    //해당 오브젝트가 보스인지
+    private bool isBoss;
+
     public EnemyAttackState(EnemyStateMachine ememyStateMachine) : base(ememyStateMachine)
     {
+
     }
     public override void Enter()
     {
         stateMachine.MovementSpeedModifier = 0;
         base.Enter();
-        StartAnimation(stateMachine.Enemy.AnimationData.AttackParameterHash);
-        StartAnimation(stateMachine.Enemy.AnimationData.BaseAttackParameterHash);
+
+        switch (stateMachine.Enemy.MonsterType)
+        {
+            case MonsterType.Normal:
+                StartAnimation(stateMachine.Enemy.AnimationData.AttackParameterHash);
+                StartAnimation(stateMachine.Enemy.AnimationData.BaseAttackParameterHash);
+                break;
+
+            case MonsterType.Boss:
+                isBoss = true;
+                EnemySkillBase bossSkill = stateMachine.Enemy.EnemySkill;
+
+                StartAnimation(stateMachine.Enemy.AnimationData.AttackParameterHash);
+
+                //스킬이 사용가능하다면
+                if(bossSkill.Skill01Ready)
+                {
+                    //스킬 사용
+                    stateMachine.MovementSpeedModifier = 0.5f;
+                    bossSkill.UseSkill(1);
+                    StartAnimation(stateMachine.Enemy.AnimationData.Skill01ParameterHash);
+                    return;
+                }
+
+                else if (bossSkill.Skill02Ready)
+                {
+                    //스킬 사용
+                    bossSkill.UseSkill(2);
+                    StartAnimation(stateMachine.Enemy.AnimationData.Skill02ParameterHash);
+                    return;
+                }
+
+                //스킬 사용이 불가능하다면
+                else
+                {
+                    //기본공격
+                    StartAnimation(stateMachine.Enemy.AnimationData.BaseAttackParameterHash);
+                }
+                break;
+        }
     }
 
     public override void Exit()
@@ -22,6 +65,8 @@ public class EnemyAttackState : EnemyBaseState
         base.Exit();
         StopAnimation(stateMachine.Enemy.AnimationData.AttackParameterHash);
         StopAnimation(stateMachine.Enemy.AnimationData.BaseAttackParameterHash);
+        StopAnimation(stateMachine.Enemy.AnimationData.Skill01ParameterHash);
+        StopAnimation(stateMachine.Enemy.AnimationData.Skill02ParameterHash);
     }
 
     public override void Update()
