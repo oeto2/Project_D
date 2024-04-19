8c482bbf53277dba1c2749fa4a2b460f4fc34190
oeto2 <oeto2@naver.com>
2024-04-19 15:31:26 +0900

    Fix : 리워드창에서 포션이 최대값으로 합쳐지는 버그 수정

--- Assets/Scripts/Inventory/Reward.cs
+++ Assets/Scripts/Inventory/Reward.cs
@@ -39,19 +39,38 @@ public class Reward : MonoBehaviour
         _rewardBase.SetActive(false);
     }
 
-    public void AcquireItem(ItemData _item, int _count = 1)
+    public void AcquireItem(ItemData item_, int count_ = 1)
     {
-        if (_item.itemType == Constants.ItemType.Consume || _item.itemType == Constants.ItemType.Material)
+        if (item_.itemType == Constants.ItemType.Consume || item_.itemType == Constants.ItemType.Material)
         {
             for (int i = 0; i < slots.Length; i++)
             {
                 if (slots[i].item != null)
                 {
-                    if (slots[i].item.itemName == _item.itemName)
+                    if (slots[i].item.itemName == item_.itemName)
                     {
-                        slots[i].SetSlotCount(_count);
+                        if (slots[i].itemCount + count_ > item_.itemMax_Stack)
+                        {
+                            int _overCount = slots[i].itemCount + count_ - item_.itemMax_Stack;
+                            slots[i].SetSlotCount(count_);
+                            count_ = _overCount;
+                            continue;
+                        }
+                        slots[i].SetSlotCount(count_);
                         return;
+                        //InformationManager.Instance.SaveInformation(i, _item.id, _count);
                     }
+                }
+                else
+                {
+                    if (count_ > item_.itemMax_Stack)
+                    {
+                        slots[i].AddItem(item_, item_.itemMax_Stack);
+                        count_ = count_ - item_.itemMax_Stack;
+                        continue;
+                    }
+                    slots[i].AddItem(item_, count_);
+                    return;
                 }
             }
         }
@@ -60,10 +79,13 @@ public class Reward : MonoBehaviour
         {
             if (slots[i].item == null)
             {
-                slots[i].AddItem(_item, _count);
+                slots[i].AddItem(item_, count_);
+                //InformationManager.Instance.SaveInformation(i, _item.id, _count);
                 return;
             }
         }
+        var _warningPopup = UIManager.Instance.GetPopup(nameof(WarningPopup)).GetComponent<WarningPopup>();
+        _warningPopup.SetWarningPopup("아이템이 가득찼습니다.");
     }
 
     //현재 리워드 인벤에 있는 아이템목록 ID값 얻기
