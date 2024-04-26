9a547fbe9290935f7eb47b25a901a3bf1084987b
oeto2 <oeto2@naver.com>
2024-04-26 11:39:49 +0900

    Feat : 미믹 버그 수정
    1. 데미지 버그
    2. 상호작용 버그

--- Assets/Scripts/Enemy/Mimic.cs
+++ Assets/Scripts/Enemy/Mimic.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class Mimic : MonoBehaviour, IInteractable
@@ -11,7 +12,6 @@ public class Mimic : MonoBehaviour, IInteractable
     [SerializeField] private Animator _animator;
 
     [SerializeField] private Enemy _enemy;
-    [SerializeField] private EnemyForceReceiver _enemyForceReceiver;
     [SerializeField] private CharacterController _characterController;
     [SerializeField] private NavMeshAgent _navMeshAgent;
     [SerializeField] private BoxCollider _boxColider;
@@ -24,16 +24,16 @@ public class Mimic : MonoBehaviour, IInteractable
         _uiManager = UIManager.Instance;
         _animator = GetComponent<Animator>();
         _enemy = GetComponent<Enemy>();
-        _enemyForceReceiver = GetComponent<EnemyForceReceiver>();
         _characterController = GetComponent<CharacterController>();
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _boxColider = GetComponent<BoxCollider>();
     }
 
+  
+
     private void Start()
     {
         _interationPopup = _uiManager.GetPopup(nameof(interationPopup)).GetComponent<interationPopup>();
-
         _loadingBar = _interationPopup.LoadingBar;
     }
 
@@ -82,7 +82,6 @@ public class Mimic : MonoBehaviour, IInteractable
     private void ActiveMimic()
     {
         _enemy.enabled = true;
-        _enemyForceReceiver.enabled = true;
         _characterController.enabled = true;
         _navMeshAgent.enabled = true;
         _boxColider.enabled = false;
