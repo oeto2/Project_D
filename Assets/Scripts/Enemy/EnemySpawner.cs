3c3de54fadeff92695209aaaa34f64fa2ef25c70
oeto2 <oeto2@naver.com>
2024-11-29 10:22:47 +0900

    Feat : 몬스터 스폰 방식 변경
    콜라이더 충돌을 통해 소환하도록 수정함

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -12,10 +12,20 @@ public class EnemySpawner : MonoBehaviour
     //생성된 몬스터 리스트
     [SerializeField] private List<GameObject> _spawnMonsters;
 
-    private void Start()
+    //변수 : 최초 1회 실행을 위한 플래그
+    private bool _isWork = false;
+
+    //콜라이더 충돌 시
+    private void OnTriggerEnter(Collider other)
     {
-        // SpawnMonsters(); //몬스터 생성
-        // SettingMonsters(); //몬스터 세팅 
+        //플레이어 충돌 시
+        if (other.gameObject.CompareTag("Player") && !_isWork)
+        {
+            SpawnMonsters(); // 몬스터 소환
+            SettingMonsters(); // 몬스터 세팅
+            _isWork = true; // 중복 실행 방지
+            Debug.Log("플레이어 충돌함");
+        }
     }
 
     //몬스터 생성
@@ -23,6 +33,7 @@ public class EnemySpawner : MonoBehaviour
     {
         for (int i = 0; i < _monsterSpawnPoints.Count; i++)
         {
+            //스폰 정보를 받아와서, 해당 종류의 몬스터를 소환함
             SpawnInfomation spawnInfomation = _monsterSpawnPoints[i].GetComponent<SpawnInfomation>();
             _spawnMonsters.Add(ResourceManager.Instance.Instantiate(spawnInfomation.GetSpawnMonsterPath(), _monsterSpawnPoints[i]));
             Enemy enemy = _spawnMonsters[i].GetComponent<Enemy>();
@@ -42,7 +53,7 @@ public class EnemySpawner : MonoBehaviour
 
             if (enemy == null)
                 continue;
-            if (enemy.MonsterMoveType == Constants.MonsterMoveType.Lock )
+            if (enemy.MonsterMoveType == Constants.MonsterMoveType.Lock)
                 continue;
 
             enemy.MonsterWanderDestination.Add(_EnemyPatrolLocations[locationNum++]);
